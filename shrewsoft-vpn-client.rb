cf4ea9eee913f48d8381a81f14137ab0c0c776e9
Yakiv Yereskovskyi <39760199+yakivy@users.noreply.github.com>
2019-06-07 17:29:15 +0300

    Update shrewsoft-vpn-client.rb

--- shrewsoft-vpn-client.rb
+++ shrewsoft-vpn-client.rb
@@ -62,7 +62,7 @@ class ShrewsoftVpnClient < Formula
 
   plist_options :manual => "sudo #{HOMEBREW_PREFIX}/sbin/iked", :startup => true
 
-  def plist; <<-EOF.undent
+  def plist; <<-EOF
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
